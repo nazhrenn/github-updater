dae5482faab37fa5c084d9eb8d5188697ed9cb71
Tyler Coles <tylerevancoles@gmail.com>
2015-11-24 21:28:12 -0500

    fixed derp

--- modules/github-api.js
+++ modules/github-api.js
@@ -6,7 +6,7 @@ module.exports = (function () {
 	var gitHubApiUrl = "https://api.github.com/";
 
 	obj.getCommits = function (user, repo, sinceDate, callback) {
-		restCall.get(gitHubApiUrl + 'repos/' + user + '/' + repo + '/commits' + (sinceDate !== null ? 'since=' + sinceDate.toISOString() : ''), null, callback);
+		restCall.get(gitHubApiUrl + 'repos/' + user + '/' + repo + '/commits' + (sinceDate !== null ? '?since=' + sinceDate.toISOString() : ''), null, callback);
 	};
 
 	return obj;
