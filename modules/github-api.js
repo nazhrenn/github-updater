1de7d19c848ba9029e43cf686439caa86d4db3da
Tyler Coles <tylerevancoles@gmail.com>
2015-11-24 21:38:39 -0500

    Added get repos for users and orgs

--- modules/github-api.js
+++ modules/github-api.js
@@ -4,9 +4,21 @@ module.exports = (function () {
 	var obj = {};
 	
 	var gitHubApiUrl = "https://api.github.com/";
+	
+	var makeGitHubUrl = function (path) {
+		return gitHubApiUrl + path;
+	};
 
 	obj.getCommits = function (user, repo, sinceDate, callback) {
-		restCall.get(gitHubApiUrl + 'repos/' + user + '/' + repo + '/commits' + (sinceDate !== null ? '?since=' + sinceDate.toISOString() : ''), null, callback);
+		restCall.get(makeGitHubUrl('repos/' + user + '/' + repo + '/commits' + (sinceDate !== null ? '?since=' + sinceDate.toISOString() : '')), null, callback);
+	};
+	
+	obj.getUserRepos = function (user, callback) {
+		restCall.get(makeGitHubUrl('/users/' + user + '/repos'), null, callback);
+	};
+	
+	obj.getOrganizationRepos = function (org, callback) {
+		restCall.get(makeGitHubUrl('/orgs/' + org + '/repos'), null, callback);
 	};
 
 	return obj;
